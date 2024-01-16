273c10b78ac7a0b22ffb5b7942bb386b76c5e2f8
Isaac Berez <isaaceberez@gmail.com>
2024-01-16 17:04:32 +0200

    add find latest sample file function

--- sample_reader.py
+++ sample_reader.py
@@ -13,8 +13,12 @@ from pandas_ods_reader import read_ods
 from copy import deepcopy
 import pprint
 import json
+import re
+from datetime import datetime
+import logging
 
 def process_meta_data(analysis_dir, dimorph_sample_file, output_folder=None):
+    '''process meta data from ods and return sample id list and full meta data dictionary'''
     #read in dimorph sample file
     dimorph_df = read_ods(analysis_dir+dimorph_sample_file)
     #fill in missing data types
@@ -32,6 +36,8 @@ def process_meta_data(analysis_dir, dimorph_sample_file, output_folder=None):
         dtype.append(d.split(":")[1])
     #get and return sample id
     samples = [x for x in list(dimorph_df['SampleID:string']) if x != None]
+    #capitilize to match folder structure
+    samples = [x.upper() for x in samples]
     #create meta data dictionary from sample ids
     meta_data_dict = dict.fromkeys(samples)
     #initialize headers for each sample
@@ -44,3 +50,44 @@ def process_meta_data(analysis_dir, dimorph_sample_file, output_folder=None):
     
     return samples, meta_data_dict
 
+def find_latest_sample_file(sample_dir, samples, sample_ind):
+    '''finds latest file denoted by YYMMDD in filename'''
+    
+    path = os.path.join(sample_dir, samples[sample_ind])
+    print ('path: ', path)
+    print ('sample ind: ',samples[sample_ind])
+    files = os.listdir(path)
+    print ('files', files)
+
+    date_list = []
+    #isolate date YYMMDD
+    for file in files:
+        #print (file)
+        if 'out' in file:
+            date = re.search("(\d{2}\d{2}\d{2})", file)
+        #print((date[0]))
+            date_list.append(date[0])
+    print (date_list)
+    
+    #add 20 in front to make YYYYMMDD, convert to datetime object, then use max() to get latest date
+    new_date_list = []
+    for d in date_list:
+        d = '20' + d
+        dt = datetime.strptime(d,'%Y%m%d')
+        new_date_list.append(dt)
+    print ('datetime dates:' , new_date_list)
+    latest_date = max(new_date_list)
+    print ('latest date: ', latest_date)
+
+    #convert latest date back to old file naming convention 
+    latest_date = str(latest_date)
+    latest_date_key = str(latest_date[2:-9])
+    latest_date_key = latest_date_key[:2]+latest_date_key[3:5]+latest_date_key[6:8]
+    print ('latest date key: ',latest_date_key)
+
+    #match latest date key with file
+    for file in files:
+        if latest_date_key in file:
+            if 'out' in file:
+                print ('latest file: ', file)
+                return file
